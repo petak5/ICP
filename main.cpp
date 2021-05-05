f0b4f4ce9213b0e704d2dbfce7a0e834620c602a
petak5 <peterurgos12345@gmail.com>
2021-05-05 22:57:12 +0200

    Hello MQTT

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <string>
+#include <mqtt/client.h>
 
 int main(int argc, char *argv[])
 {
+    mqtt::client client("localhost:1883", "ICP_project");
+    try {
+        client.connect();
+
+        auto msg = "Hello MQTT";
+        client.publish("ICP_main_topic", msg, strlen(msg), 0, false);
+
+        client.disconnect();
+    }
+    catch (const mqtt::exception& exc) {
+        std::cerr << "Error: " << exc.what() << std::endl;
+        return 1;
+    }
+
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
